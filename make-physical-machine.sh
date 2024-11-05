23fffbb0af6e640c711257538515753522a976f2
Richard W.M. Jones <rjones@redhat.com>
2024-11-05 10:22:33 +0000

    make-physical-machine.sh: Use fedora-41 and reformat comments
    Move the baseline for the physical machine to fedora-41, and reflow
    all the comments.

--- make-physical-machine.sh
+++ make-physical-machine.sh
@@ -16,6 +16,27 @@
 
 set -e -u -C
 
+base_image=fedora-41
+
+# The array below open-codes the partition:filesystem layout of the
+# virt-builder disk template used above. Whenever you bump the Fedora
+# version, update the array below as needed.
+#
+# We cannot use inspection either before or after the filesystem UUID
+# changes:
+#
+# - the UUID of a mounted filesystem cannot be changed (at least with
+# - XFS);
+#
+# - right after the UUID changes, inspection does not work correctly,
+#   as the new fs UUIDs are out of sync with the UUID references in
+#   those config files that inspection relies upon.
+#
+# Note that the order of entries is important too: the mount points
+# must be listed in dependency order (put the dependees first, the
+# dependants last).
+fsdevs=(/dev/sda3:/ /dev/sda2:/boot)
+
 GUESTFISH_PID=
 disk=
 
@@ -39,41 +60,26 @@ outdir=$(dirname -- "$output")
 disk=$(mktemp -p "$outdir" physical-machine.tmp.XXXXXXXXXX)
 # Delay the SELinux relabeling.
 virt-builder --format raw -o "$disk" --root-password password:p2v-phys \
-  --no-selinux-relabel fedora-39
+  --no-selinux-relabel $base_image
 
-# Start a guestfish server on the disk image, so that each of the several
-# UUID-manipulation commands below not need a separate guestfish launch.
+# Start a guestfish server on the disk image, so that each of the
+# several UUID-manipulation commands below not need a separate
+# guestfish launch.
 guestfish_set_pid=$(guestfish --listen --format=raw --add "$disk")
 eval "$guestfish_set_pid"
 guestfish --remote -- run
-
-# The array below open-codes the partition:filesystem layout of the virt-builder
-# disk template used above. Whenever you bump the Fedora version, update the
-# array below as needed.
-#
-# We cannot use inspection either before or after the filesystem UUID changes:
-#
-# - the UUID of a mounted filesystem cannot be changed (at least with XFS);
-#
-# - right after the UUID changes, inspection does not work correctly, as the new
-#   fs UUIDs are out of sync with the UUID references in those config files that
-#   inspection relies upon.
-#
-# Note that the order of entries is important too: the mount points must be
-# listed in dependency order (put the dependees first, the dependants last).
-fsdevs=(/dev/sda3:/ /dev/sda2:/boot)
 
 # For each filesystem:
 #
 # - regenerate the UUID,
 #
-# - produce a sed command (scriptlet) that performs the same UUID replacement in
-#   a text file,
+# - produce a sed command (scriptlet) that performs the same UUID
+#   replacement in a text file,
 #
 # - mount the filesystem.
 #
-# Note that later we're going to rely on the fact that the generated sed
-# commands *require no quoting* on the shell command line.
+# Note that later we're going to rely on the fact that the generated
+# sed commands *require no quoting* on the shell command line.
 declare -a sed_script
 sed_idx=0
 for fsdev in "${fsdevs[@]}"; do
@@ -94,8 +100,8 @@ done
 refresh_uuid_refs=(find /boot /etc -type f -print0 '|'
                    xargs -0 -r -- sed -i "${sed_script[@]}" --)
 
-# Passing the shell command to the appliance is where we rely on the fact that
-# the sed commands for replacing UUIDs require no quoting.
+# Passing the shell command to the appliance is where we rely on the
+# fact that the sed commands for replacing UUIDs require no quoting.
 guestfish --remote -- sh "${refresh_uuid_refs[*]}"
 
 # Tear down the guestfish server before we use virt-customize.
@@ -106,12 +112,14 @@ while kill -s 0 -- "$waitpid" 2>/dev/null; do
   sleep 1
 done
 
-# Reapply the SELinux labels now. Use virt-customize for this, rather than
-# guestfish's "selinux-relabel", as virt-customize contains some heavy logic
-# related to "specfile". Inspection does work here, because the config files are
-# in sync again with the filesystem UUIDs.
+# Reapply the SELinux labels now. Use virt-customize for this, rather
+# than guestfish's "selinux-relabel", as virt-customize contains some
+# heavy logic related to "specfile". Inspection does work here,
+# because the config files are in sync again with the filesystem
+# UUIDs.
 virt-customize --format raw --add "$disk" --selinux-relabel
 
-# We're done; rename the temporary disk image to the expected output file.
+# We're done; rename the temporary disk image to the expected output
+# file.
 mv -- "$disk" "$output"
 disk=
